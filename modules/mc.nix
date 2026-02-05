ebf17a01d1d1db2e136a6960a45a0d7939775ff5
netlooker <netlooker@outlook.be>
2026-02-05 21:41:09 +0100

    🎨 Theme Fix: Relocate mc skin and provide full 256-color definition

--- modules/mc.nix
+++ modules/mc.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 
 {
-  xdg.configFile."mc/skins/catppuccin-mocha.ini".source = ./skins/catppuccin-mocha.ini;
+  # Place the skin in the standard local path where mc definitely looks
+  home.file.".local/share/mc/skins/catppuccin-mocha.ini".source = ./skins/catppuccin-mocha.ini;
   
   home.sessionVariables = {
     # Set the default skin for Midnight Commander
